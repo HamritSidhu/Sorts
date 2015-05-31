81811b2c24ef5da445dfa1029092cdc587f0bd65
HamritSidhu <hamritsidhu_94@hotmail.com>
2015-05-31 17:00:59 -0400

    implemented bubble sort

---
+++ sorts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 1, 5, 7, 9, 3, 6, 17, 12}; //sample list to sort
+
+            var bubbleSort = new _bubblesort();
+            int[] bSortedList = bubbleSort.performSort(list);
+
+            string bOutput = "Bubble Sorted Output: ";
+            for (int i = 0; i < bSortedList.Length; i++) {
+                bOutput += bSortedList[i] + " ";
+            }
+
+            Console.WriteLine(bOutput);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ sorts/_bubblesort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorts
+{
+    class _bubblesort : evaluationInterface
+    {
+        public int[] performSort(int[] list) {
+            int swapCount = 0;
+            int temp = 0;
+            while (true)
+            {
+                swapCount = 0;
+                for (int i = 0; i < list.Length - 1; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        temp = list[i + 1];
+                        list[i + 1] = list[i];
+                        list[i] = temp;
+                        swapCount++;
+                    }
+                }
+                if (swapCount == 0)
+                    break;
+            }
+            return list;
+        }
+    }
+}
